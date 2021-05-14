7418b2265c9f1cb2fac14103ab2c34bb31ce8c54
mhidano <mhidano0914@gmain.com>
2021-05-15 00:44:08 +0900

    Lesson6のChapter7.5を実装

--- taskapp/AppDelegate.swift
+++ taskapp/AppDelegate.swift
@@ -9,7 +9,7 @@ import UIKit
 import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
 
 
@@ -20,7 +20,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
             // Enable or disable features based on authorization
         }
+        center.delegate = self
+        
         return true
+    }
+    
+    // アプリがフォアグラウンドの時に通知を受け取ると呼ばれるメソッド
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .list, .sound])
     }
 
     // MARK: UISceneSession Lifecycle
